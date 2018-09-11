367cd7536838c169797c6cbe03d2a33017623109
Pavel Boldin <boldin.pavel@gmail.com>
2018-09-12 00:34:33 +0300

    it works!
    Signed-off-by: Pavel Boldin <boldin.pavel@gmail.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 
-poormanbts: poormanbts.o
+CPPFLAGS += -Ilibcare/src
+LDLIBS += $(shell pkg-config --libs libunwind libunwind-ptrace)
 
-.PHONY: libcare
+poormanbts: poormanbts.o 		\
+	libcare/src/kpatch_process.o 	\
+	libcare/src/kpatch_log.o	\
+	libcare/src/kpatch_ptrace.o	\
+	libcare/src/kpatch_coro.o	\
+	libcare/src/kpatch_elf.o
 
-libcare:
+libcare/%:
 	make -C libcare
--- poormanbts.c
+++ poormanbts.c
@@ -5,11 +5,19 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/user.h>
+
+#include "eflags.h"
+
+#include "kpatch_process.h"
+#include "kpatch_log.h"
 
 struct jump_op {
 #define JUMP_OP_OPCODE_DYNAMIC	0xff
@@ -35,6 +43,7 @@ static int parse_jump_op(struct jump_op *jcc,
 	int ret, len;
 	const char *p = in;
 
+	memset(jcc, 0, sizeof(*jcc));
 	ret = sscanf(p, "0x%x 0x%lx+0x%x %n",
 		     &jcc->opcode,
 		     &jcc->from,
@@ -49,7 +58,6 @@ static int parse_jump_op(struct jump_op *jcc,
 	if (ret == 1)
 		return 0;
 
-	jcc->to = 0;
 	ret = sscanf(p, "*0x%x(%d, %d, %d)",
 		     &jcc->dynamic_disp32,
 		     &jcc->dynamic_reg,
@@ -144,19 +152,12 @@ out_err:
 	return ret;
 }
 
-static int install_trace_points(int pid, struct tracepoint *tpoints, size_t npoints)
+static int install_trace_points(kpatch_process_t *child, struct tracepoint *tpoints, size_t npoints)
 {
-	int memfd = -1, ret = -1;
+	int memfd = child->memfd, ret = -1;
 	char path[1024];
 	struct tracepoint *p;
 	size_t i;
-
-	sprintf(path, "/proc/%d/mem", pid);
-	memfd = open(path, O_RDWR);
-	if (memfd < 0) {
-		perror("open");
-		goto out;
-	}
 
 	for (i = 0, p = tpoints; i < npoints; i++, p++) {
 		ret = pwrite(memfd, "\xcc", 1, p->jcc.from);
@@ -166,50 +167,166 @@ static int install_trace_points(int pid, struct tracepoint *tpoints, size_t npoi
 
 	ret = 0;
 out:
-	if (memfd != -1)
-		close(memfd);
 	return ret;
 }
 
-static int attach_to_process(char * const *argv)
+static int attach_to_process(kpatch_process_t *child, char * const *argv)
 {
 	int ret, status;
 	pid_t pid, waited;
+	int sv[2];
+
+	ret = socketpair(AF_LOCAL, SOCK_STREAM, 0, sv);
+	if (ret < 0) {
+		perror("socketpair");
+		return ret;
+	}
 
 	pid = fork();
+
 	if (pid < 0) {
 		perror("fork");
 		return -1;
 	} else if (pid == 0) {
-		ptrace(PTRACE_TRACEME, 0, 0, 0);
-		return execvp(argv[0], argv + 1);
+		recv(sv[0], &ret, sizeof(ret), 0);
+		asm (".align 8; int $3; .align 8");
+		return execvp(argv[0], argv);
 	}
 
-	ret = ptrace(PTRACE_ATTACH, pid, NULL, NULL);
-	if (ret < 0) {
-		perror("ptrace");
-		return -1;
-	}
-
-	while (1) {
-		waited = waitpid(pid, &status, 0);
-
-		if (WIFSTOPPED(status) &&
-		    WSTOPSIG(status) == SIGSTOP)
-			break;
-
-		ret = ptrace(PTRACE_CONT, pid, NULL,
-			     (void *)(uintptr_t)WTERMSIG(status));
-		if (ret < 0) {
-			perror("ptrace");
-		}
-	}
-
-	return pid;
+	kpatch_process_init(child, pid, 1, sv[1]);
+	kpatch_process_load_libraries(child);
 }
 
-static int start_process(int pid)
+unsigned long reg_to_offset[] = {
+#define REG(x)	offsetof(struct user_regs_struct, x)
+	[0]	=	REG(rax),
+	[1]	=	REG(rcx),
+	[2]	=	REG(rdx),
+	[3]	=	REG(rbx),
+	[4]	=	REG(rsp),
+	[5]	=	REG(rbp),
+	[6]	=	REG(rsi),
+	[7]	=	REG(rdi),
+	[32]	=	REG(rip),
+#undef REG
+};
+
+
+
+static bool trace_point_check_condition(int pid, struct tracepoint *tpoint)
 {
+	unsigned long eflags;
+
+	eflags = ptrace(PTRACE_PEEKUSER, pid, offsetof(struct user_regs_struct, eflags), NULL);
+
+#define	FLAG(x)	(eflags & X86_EFLAGS_ ## x)
+	switch (tpoint->jcc.opcode) {
+	case	0x7f: /* jg */
+		return	!FLAG(ZF) && FLAG(SF) == FLAG(OF);
+
+	case	0x74: /* je */
+		return	FLAG(ZF);
+
+	case	0x76: /* jbe */
+		return	FLAG(CF) || FLAG(ZF);
+
+	case	0x73: /* jae */
+		return	!FLAG(CF);
+
+	case	0x75: /* jne */
+		return	!FLAG(ZF);
+
+	case	0xe9:
+	case	0xeb:
+	case	JUMP_OP_OPCODE_DYNAMIC:
+		/* unconditional jumps */
+		return true;
+	default:
+		printf("unknown opcode %x\n", tpoint->jcc.opcode);
+		abort();
+	}
+}
+
+static long trace_point_resolve_to(int pid, long rip, struct tracepoint *tpoint)
+{
+	bool is_ref = tpoint->jcc.dynamic_reg & JUMP_OP_DYNAMIC_REG_REF;
+	bool is_sib = tpoint->jcc.dynamic_sib_mult; 
+	int reg = tpoint->jcc.dynamic_reg & ~JUMP_OP_DYNAMIC_REG_REF;
+
+	if (tpoint->jcc.opcode != JUMP_OP_OPCODE_DYNAMIC) {
+		return tpoint->jcc.to;
+	}
+
+	if (!is_ref) {
+		long off = reg_to_offset[reg];
+		return ptrace(PTRACE_PEEKUSER, pid, off, NULL);
+	}
+
+	if (!is_sib) {
+		long off;
+		if (reg == 32)  /* RIP */
+			off = rip;
+		else
+			off = ptrace(PTRACE_PEEKUSER, pid, reg_to_offset[reg], NULL);
+		off += tpoint->jcc.dynamic_disp32;
+		printf("off = %lx\n", off);
+		return ptrace(PTRACE_PEEKDATA, pid, off, NULL);
+	}
+
+	printf("BROKEN!\n");
+	abort();
+}
+
+static void trace_point_execute(int pid, long rip, struct tracepoint *tpoint)
+{
+	long rv;
+
+	rip += tpoint->jcc.len;
+	if (trace_point_check_condition(pid, tpoint))
+		rip = trace_point_resolve_to(pid, rip, tpoint);
+
+	printf("from = %lx, opcode = %x, to = %lx\n", tpoint->jcc.from, tpoint->jcc.opcode, rip);
+
+	rv = ptrace(PTRACE_POKEUSER, pid,
+		    offsetof(struct user_regs_struct, rip),
+		    (void *)(uintptr_t)rip);
+
+	if (rv == -1) {
+		kplogerror("wtf?");
+	}
+}
+
+static int trace_process(kpatch_process_t *child,
+			 struct tracepoint *tpoints,
+			 size_t npoints)
+{
+	int pid;
+	long rv;
+	size_t i;
+	
+	while (1) {
+		pid = kpatch_process_execute_until_stop(child);
+
+		printf("pid = %d\n", pid);
+		rv = ptrace(PTRACE_PEEKUSER, pid,
+			    offsetof(struct user_regs_struct, rip),
+			    NULL);
+		if (rv == -1) {
+			perror("ptrace");
+			return rv;
+		}
+		rv--;
+
+		for (i = 0; i < npoints; i++) {
+			if (rv == tpoints[i].jcc.from) {
+				trace_point_execute(pid, rv, &tpoints[i]);
+				break;
+			}
+		}
+		if (i == npoints) {
+			printf("Unexpected stop at %lx\n", rv);
+		}
+	}
 }
 
 int main(int argc, char * const argv[])
@@ -218,15 +335,16 @@ int main(int argc, char * const argv[])
 	size_t npoints;
 	int ret;
 	int pid;
+	kpatch_process_t child;
 
 	ret = read_input_file(argv[1], &tpoints, &npoints);
 	if (ret < 0)
 		return -1;
 
-	pid = attach_to_process(argv + 2);
-	ret = install_trace_points(pid, tpoints, npoints);
+	pid = attach_to_process(&child, argv + 2);
+	ret = install_trace_points(&child, tpoints, npoints);
 	if (ret < 0) {
 		fprintf(stderr, "install_trace_points\n");
 	}
-	start_process(pid);
+	trace_process(&child, tpoints, npoints);
 }
